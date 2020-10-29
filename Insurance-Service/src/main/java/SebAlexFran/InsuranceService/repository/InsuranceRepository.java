1a9b8e7305f6c5a569b7dffc01773c73a188e334
Franfran <francwan98@hotmail.fr>
2020-10-29 10:30:07 +0100

    RepositoryMongo change

--- Insurance-Service/src/main/java/SebAlexFran/InsuranceService/repository/InsuranceRepository.java
+++ Insurance-Service/src/main/java/SebAlexFran/InsuranceService/repository/InsuranceRepository.java
@@ -4,7 +4,8 @@ import SebAlexFran.InsuranceService.model.Insurance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
-public interface InsuranceRepository extends JpaRepository<Insurance, Long> {
+public interface InsuranceRepository extends MongoRepository<Insurance, String> {
 
 }
